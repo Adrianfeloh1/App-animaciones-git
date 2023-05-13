f9420ae5537d533b5aed36f5fea03e36f4e51ed4
Adrian Ochoa <adrianfeloh1@gmail.com>
2023-05-12 19:20:22 -0500

    Cargando el componente del input AddCategory con sus validaciones

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,20 +1,34 @@
 import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState([
+    "one puch",
+    " Deamon Slayer",
+    " Dragon Ball Z",
+  ]);
 
-  const [categories, setCategories] = useState(["one puch", " Deamon Slayer", " Dragon Ball Z"]);
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+
+    setCategories([newCategory, ...categories]);
+  };
 
   return (
     <>
       <h1> GifExpertApp</h1>
 
+      <AddCategory /* setCategories={setCategories} categories={categories} */
+        onNewCategory={(event) => onAddCategory(event)}
+      />
+
+      <button onClick={onAddCategory}>Agregar</button>
+
       <ul>
-        {categories.map ( category => {
-            return <li key={category}> {category}</li>
+        {categories.map((category) => {
+          return <li key={category}> {category}</li>;
         })}
       </ul>
-
-
     </>
   );
 };
